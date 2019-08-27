2841192816b51f0fd228689caec07e09a2da4cce
popshia <B123292179@gmail.com>
2019-08-27 21:06:26 +0800

    update

--- ObjectTracking.py
+++ ObjectTracking.py
@@ -19,6 +19,7 @@ ret = cap.set(4,480)             ##change to 320X240
 cap.set(propId,value)
 propId可以是0到46之間的任何数，每一個數代表一个個屬性，自己可以嘗試一下
 '''
+
 while(1):
  # Take each frame
  _, frame = cap.read()
@@ -27,6 +28,7 @@ while(1):
  # Convert BGR to HSV
  frame2=frame.copy()
  frame = cv2.GaussianBlur(frame,(77,77),0)
+ 
  '''
  cv2.GaussianBlur(src, ksize, sigmaX[, dst[, sigmaY[, borderType]]])  
  src: 影象矩陣 
@@ -42,16 +44,19 @@ while(1):
  upper_green = np.array([80,255,255])
  # Threshold the HSV image to get only green colors
  mask = cv2.inRange(hsv, lower_green, upper_green)
+ 
  '''
  hsv指的是原圖
  lower_green指的是圖像中低於這個lower_green的值，圖像值變為0
  upper_green指的是圖像中高於這個upper_green的值，圖像值變為0
  '''
+ 
  mask_org=mask.copy()
 
  # Bitwise-AND mask and original image
  res = cv2.bitwise_and(frame,frame, mask= mask)
  #不知道mask = mask意思
+ 
  '''
  cv2.bitwise_and(src1, src2[, dst[, mask]])
  bitwise_and是對二進位制資料進行“與”操作，即對影象（灰度影象或彩色影象均可）每個畫素值進行二進位制
@@ -60,7 +65,6 @@ while(1):
  src – single input array.
  dst – output array that has the same size and type as the input arrays.
  mask – optional operation mask, 8-bit single channel array, that specifies elements of the output array to be changed.
-
  '''
  
  ##=================================
